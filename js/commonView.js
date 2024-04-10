c98d3a442db4ab80c92179e493d76825357adb11
sofus64 <157691346+sofus64@users.noreply.github.com>
2024-04-10 12:31:54 +0200

    header test

---
+++ js/commonView.js
@@ -0,0 +1,24 @@
+function createHeaderHtml() {
+    let html = /*HTML*/ `
+    <div class="headerFull">
+
+        <div class="headerRight, headerItem">
+            <button>Hjem</button>
+            <button>Chat</button>
+            <button>Aktiviteter</button>
+        </div>
+
+        <div class="headerCenter, headerItem">
+            <h1>Meowtch!<h1>
+        </div>
+
+        <div class="headerRight, headerItem">
+            <button>Profil</button>
+            <button>Innstillinger</button>
+        </div>
+
+    </div>
+    `;
+
+    return html;
+}
