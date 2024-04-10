e51b7c4afa53be2242ebbbcc204a56aeb5f626dc
Chris <oscar.sletvold@gmail.com>
2024-04-10 13:47:11 +0200

    update

--- js/homepage/homepageView.js
+++ js/homepage/homepageView.js
@@ -2,18 +2,21 @@ function homepageView() {
     const app = document.getElementById('app');
     const registeredUsers = model.data.registeredUsers;
     const index = model.inputs.mainPage.currentProfileIndex;
-    let html = '';
+     
+    
 
 
     app.innerHTML = /*HTML*/ `
-    <div class="container">
+    <div class="swipePageContainer">
         ${createHeaderHtml()}
         <div>
             <h3>${registeredUsers[index].displayName}</h3>
             <div class="profileCard">
                 <button></button>
-                <img id="profileImg" src="${registeredUsers[index].uploadedImgs[1]}" alt="profile picture">
-                <p id="displayBio"></p>
+                <div class="img-container">
+                <img id="profileImg" src="${registeredUsers[index].uploadedImgs[registeredUsers[index].profilePictureIndex]}" alt="profile picture">
+                </div>
+                <p id="displayBio">${registeredUsers[index].displayBio}</p>
                 <button></button>
             </div>
         </div>
