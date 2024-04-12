bee67def5ae609e6c68ace6e8c6d609af3ae556d
Chris <oscar.sletvold@gmail.com>
2024-04-12 14:24:54 +0200

    formatering av loginView.

--- js/loginpage/loginview.js
+++ js/loginpage/loginview.js
@@ -1,62 +1,45 @@
 function loginView() {
   if (model.inputs.loginPage.registerNewUser.isActive != true) {
     document.getElementById("app").innerHTML = /*HTML*/ `
-        <div class="container">
-        
-        
+      <div class="container">
         <input 
-        type="text" 
-        oninput="model.inputs.loginPage.usernameInput=this.value" 
-        placeholder="Enter Username"/>
-        
+          type="text" 
+          oninput="model.inputs.loginPage.usernameInput=this.value" 
+          placeholder="Enter Username"/>
         <input 
-        type="password" 
-        oninput="model.inputs.loginPage.passwordInput=this.value" 
-        placeholder="Enter Password"/>
-        
+          type="password" 
+          oninput="model.inputs.loginPage.passwordInput=this.value" 
+          placeholder="Enter Password"/>
         <button onclick="login()">Login</button>
-        
         <button onclick="changeModel(true)">New User</button>
-        
-        
-        </div>
-        `;
+      </div>
+    `;
   } else {
     document.getElementById("app").innerHTML = /*HTML*/ `
-        <div class="container">
-        
-        
+      <div class="container">
         <input 
-        type="text" 
-        oninput="model.inputs.loginPage.registerNewUser.newUserNameInput=this.value" 
-        placeholder="Enter Username"/>
-
+          type="text" 
+          oninput="model.inputs.loginPage.registerNewUser.newUserNameInput=this.value" 
+          placeholder="Enter Username"/>
         <input 
-        type="text" 
-        oninput="model.inputs.loginPage.registerNewUser.newEmailInput=this.value" 
-        placeholder="Enter Email"/>
-
+          type="text" 
+          oninput="model.inputs.loginPage.registerNewUser.newEmailInput=this.value" 
+          placeholder="Enter Email"/>
         <input 
-        type="text" 
-        oninput="model.inputs.loginPage.registerNewUser.newPhoneNrInput=this.value" 
-        placeholder="Enter PhoneNumber"/>
-        
+          type="text" 
+          oninput="model.inputs.loginPage.registerNewUser.newPhoneNrInput=this.value" 
+          placeholder="Enter PhoneNumber"/>
         <input 
-        type="password" 
-        oninput="model.inputs.loginPage.registerNewUser.newPasswordInput=this.value" 
-        placeholder="Enter Password"/>
-
+          type="password" 
+          oninput="model.inputs.loginPage.registerNewUser.newPasswordInput=this.value" 
+          placeholder="Enter Password"/>
         <input 
-        type="password" 
-        oninput="model.inputs.loginPage.registerNewUser.newPasswordReInput=this.value" 
-        placeholder="Re Enter Password"/>
-        
+          type="password" 
+          oninput="model.inputs.loginPage.registerNewUser.newPasswordReInput=this.value" 
+          placeholder="Re Enter Password"/>
         <button onclick="registerNewUser()">Register</button>
-        
         <button onclick="changeModel(false)">Existing User</button>
-        
-        
-        </div>
-        `;
+      </div>
+    `;
   }
 }
